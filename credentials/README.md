15be07cee700b85ff52e916c6a343721af045e1e
Hikaru <hkak03key@gmail.com>
2021-01-05 03:48:11 +0000

    explain using ADC as `gcloud auth application-default login`

--- credentials/README.md
+++
@@ -1,5 +0,0 @@
-# Credentials
-
-For running docker-compose, please put service account key as "service_account_credentials.json" on this directory.
-
-notice: fail to access google drive api using `gcloud auth application-default login` at Jan 1st, 2021
--- examples/provider/provider.tf
+++ examples/provider/provider.tf
@@ -7,4 +7,13 @@ terraform {
 }
 
 provider "gdrive" {
+/*
+this provider authenticates using Application Default Credentials (ADC).
+see https://cloud.google.com/docs/authentication/production
+
+if using ADC as `gcloud auth application-default login`, please do as follows:
+- due to OAuth 2.0 scope, set `--scopes=https://www.googleapis.com/auth/drive,https://www.googleapis.com/auth/cloud-platform` as args
+- due to using google drive api, prepare the gcp project that is enabled google drive api and created OAuth 2.0 Client IDs and json file,
+  and set `--client-id-file=<downloaded_client_id_file>` as args
+*/
 }
